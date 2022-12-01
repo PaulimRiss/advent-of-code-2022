1680926d60dfaff14ea6b78f4f7dcd93bd517d08
Daniel Rissato <danielpprissato@gmail.com>
2022-12-01 12:53:10 -0300

    [Day 1] Update solution to generalize input treatment

--- day1/main.py
+++ day1/main.py
@@ -1,28 +1,24 @@
 def main():
     input = open("input.txt", "r").read()
-    print(get_bigger_cal_elf(input))
-    print(get_top_3_cal_elf(input))
+    elfs = simplify_to_list(input)
+    print(get_bigger_cal_elf(elfs))
+    print(get_top_3_cal_elf(elfs))
+
+
+def simplify_to_list(input):
+    res = input.split("\n\n")
+    for i in range(len(res)):
+        res[i] = sum([int(cal if cal.isnumeric() else 0) for cal in res[i].split("\n")])
+    return res
 
 
 # first part
-def get_bigger_cal_elf(input):
-    elfs = input.split("\n\n")
-    for i in range(len(elfs)):
-        elfs[i] = sum(
-            [int(cal if cal.isnumeric() else 0) for cal in elfs[i].split("\n")]
-        )
-
+def get_bigger_cal_elf(elfs):
     return max(elfs)
 
 
 # second part
-def get_top_3_cal_elf(input):
-    elfs = input.split("\n\n")
-    for i in range(len(elfs)):
-        elfs[i] = sum(
-            [int(cal if cal.isnumeric() else 0) for cal in elfs[i].split("\n")]
-        )
-
+def get_top_3_cal_elf(elfs):
     top_three = sorted(elfs, reverse=True)[:3]
 
     return sum(top_three)
